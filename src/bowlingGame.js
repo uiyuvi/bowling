af6dcb360168eed103394607dde9066561a68698
Ranganathan Yuvaraj <I46628@INTNP.SYSNP.SHARED.FORTIS>
2019-04-11 16:38:03 +0200

    refactor: Calculate score when it is requested

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,12 +1,20 @@
 var BowlingGame = function(){
     var score = 0;
+    var rolls = [];
     this.roll = function(pins){
-        score += pins;
+        rolls.push(pins);
     }
 
     this.score = function(){
+        for (let roll = 0; roll < rolls.length; roll++){
+            score += currentRollScore(rolls, roll);
+        }
         return score;
     }
 }
 
 module.exports = BowlingGame;
+
+function currentRollScore(rolls, roll) {
+    return rolls[roll];
+}
