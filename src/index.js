84f3dc83a3e082f66b62b246d24866c02ebb284e
Ranganathan Yuvaraj <I46628@INTNP.SYSNP.SHARED.FORTIS>
2019-04-12 14:59:12 +0200

    feat: Handle spare game

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ var BowlingGame = require('./bowlingGame');
 
 var bowlingGame = new BowlingGame();
 
-document.getElementById("startGame").addEventListener('click', function (e) {
-    for (let rollCount = 0;rollCount < 20; rollCount++){
-        bowlingGame.roll(1);
+document.getElementById("startGame").addEventListener('click', function (e) {    
+    bowlingGame.roll(9);
+    bowlingGame.roll(1);
+    bowlingGame.roll(5);
+    for (let rollCount = 0;rollCount < 17; rollCount++){
+        bowlingGame.roll(0);
     }
     document.getElementById("score").innerHTML = bowlingGame.score();
 });
